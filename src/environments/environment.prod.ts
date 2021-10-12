8cde219774ed2c323d5899c163b34f076df958d9
unknown <sudip@multipliersolutions.in>
2021-10-12 17:36:18 +0530

    docrankurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiurl: "https://multipliersolutions.in/gmbhospitals/gmb_api/",
-  docrankurl: "http://docrank.us/gmb/gmb_api/"
+  docrankurl: "https://docrank.us/gmb/gmb_api/"
 };
